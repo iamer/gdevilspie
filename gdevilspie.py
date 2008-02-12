87826c7b891e2d9df9d6f251160ccb06b03f9fbc
pharon <pharon@04f495f3-2145-0410-90fa-e36e85c9abab>
2008-02-12 18:23:23 +0000

    First pass of code restructuring done.

--- gdevilspie.py
+++ gdevilspie.py
@@ -16,8 +16,11 @@
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# Import needed models
+
 try:
     import os
+    import sys
     import gobject
     import pygtk
     pygtk.require('2.0')
@@ -25,12 +28,15 @@ try:
     import gtk.glade
 except:
     print "pyGTK is not correctly installed, exiting."
-    quit()
+    sys.exit(1)
 
+# List of possible match criteria
 match_criteria=["window_name", "window_role", "window_class", "window_xid", "application_name", "window_property", "window_workspace"]
 
+# List of possible actions
 actions=["geometry", "fullscreen", "focus", "center", "maximize", "maximize_vertically", "maximize_horizontally", "unmaximize", "minimize", "unminimize", "shade", "unshade", "close", "pin", "unpin", "stick", "unstick", "set_workspace", "set_viewport", "skip_pager", "skip_tasklist", "above", "below", "decorate", "undecorate", "wintype", "opacity", "spawn_async", "spawn_sync"]
 
+# Dictionary of the actions for each of which we store a dictionary of help text and widgets
 actions_dict={"geometry" : None,
 "fullscreen" : None,
 "focus": None,
@@ -59,38 +65,31 @@ actions_dict={"geometry" : None,
 "wintype": None,
 "opacity": None,
 "spawn_async": None,
-"spawn_sync": None}
+"spawn_sync": None }
 
-class gdevilspie:
+# Glade file used in all classes
+gladefile="gdevilspie.glade"
+
+# Directory where we store .ds files
+dir = os.path.expanduser("~/.devilspie")
+
+# The main class which creates the main window where we list the rules
+class RulesListWindow:
   def __init__(self):
-    self.gladefile="gdevilspie.glade"
     try:
-        self.wTreeList = gtk.glade.XML (self.gladefile, "RulesList")
+	wTreeList = gtk.glade.XML (gladefile, "RulesList")
     except:
         gtk.MessageDialog(None, 0, gtk.MESSAGE_ERROR, gtk.BUTTONS_CLOSE, "Glade file not found, exiting.").run()
         quit()
-    
-    self.wTreeEdit = gtk.glade.XML (self.gladefile, "RuleEdit")
 
-    self.RulesList = self.wTreeList.get_widget("RulesList")
-    self.RuleEdit = self.wTreeEdit.get_widget("RuleEdit")
-    self.RulesTree = self.wTreeList.get_widget("RulesTree")
-    self.MatchTree = self.wTreeEdit.get_widget("MatchTree")
-    self.ActionsTree = self.wTreeEdit.get_widget("ActionsTreeList")
-    self.MatchPropertyParameters_notebook = self.wTreeEdit.get_widget("MatchOptions_NoteBook1")
-    self.ActionsParameters_notebook = self.wTreeEdit.get_widget("ActionsParameters_notebook")
-    self.RuleName_entry = self.wTreeEdit.get_widget("RuleName_entry")
+    self.RulesList = wTreeList.get_widget("RulesList")
+    self.RulesTree = wTreeList.get_widget("RulesTree")
     
-    self.wTreeList.signal_autoconnect (self)
-    self.wTreeEdit.signal_autoconnect (self)
+    wTreeList.signal_autoconnect (self)
 
     self.rules_list_store = gtk.ListStore(str)
-    self.match_list_store = gtk.ListStore(bool, str)
-    self.actions_list_store = gtk.ListStore(bool,str)
-    
+
     self.RulesTree.set_model(self.rules_list_store)
-    self.MatchTree.set_model(self.match_list_store)
-    self.ActionsTree.set_model(self.actions_list_store)
 
     self.RulesFilesNames=gtk.TreeViewColumn('Rule Name')
     self.RulesTree.append_column(self.RulesFilesNames)
@@ -98,6 +97,68 @@ class gdevilspie:
     self.RulesFilesNames.pack_start(self.RuleFileName,expand=True)
     self.RulesFilesNames.add_attribute(self.RuleFileName, 'text', 0)
     
+    if (os.path.exists(dir)):
+      if (os.path.isdir(dir)):
+        self.fill_rules_list()
+      else:
+          print "~/.devilspie is a file, please remove it"
+    else:
+          os.makedirs(dir)
+    
+    self.RulesList.show_all()  
+  
+  def on_RulesList_destroy(self,widget):
+    gtk.main_quit()
+
+  def on_Quit_clicked(self, widget):
+    gtk.main_quit()
+
+  def on_AddRule_clicked(self,widget):
+    RuleEdit = RuleEditorWindow()
+    
+  def on_DeleteRule_clicked(self,widget):
+   SelectedRow = self.RulesTree.get_selection()
+   (model, iter) = SelectedRow.get_selected()
+   if (iter != None):
+     SelectedRule = self.rules_list_store.get(iter, 0)
+     RuleFile = os.path.expanduser("~/.devilspie/") + SelectedRule[0] + '.ds'
+     os.remove(RuleFile)
+     self.rules_list_store.remove(iter)
+
+  def update_rules_list(self):
+    self.rules_list_store.clear()
+    self.fill_rules_list()
+    
+  def fill_rules_list(self):
+    rulefileslist = os.listdir(dir)
+    for rulefile in rulefileslist:
+      if (rulefile.endswith(".ds")):
+        rulefile=gobject.filename_display_name(rulefile)
+        rulefile=rulefile.replace(".ds","")
+        self.rules_list_store.append([rulefile])
+
+class RuleEditorWindow():
+  def __init__(self):
+    try:
+	wTreeEdit = gtk.glade.XML (gladefile, "RuleEdit")
+    except:
+        gtk.MessageDialog(None, 0, gtk.MESSAGE_ERROR, gtk.BUTTONS_CLOSE, "Glade file not found, exiting.").run()
+        quit()
+    
+    self.RuleEdit = wTreeEdit.get_widget("RuleEdit")
+    self.ActionsTree = wTreeEdit.get_widget("ActionsTreeList")
+    self.MatchPropertyParameters_notebook = wTreeEdit.get_widget("MatchOptions_NoteBook1")
+    self.ActionsParameters_notebook = wTreeEdit.get_widget("ActionsParameters_notebook")
+    self.RuleName_entry = wTreeEdit.get_widget("RuleName_entry")
+    self.MatchTree = wTreeEdit.get_widget("MatchTree")
+
+    wTreeEdit.signal_autoconnect (self)
+
+    self.match_list_store = gtk.ListStore(bool, str)
+    self.actions_list_store = gtk.ListStore(bool,str)
+    self.MatchTree.set_model(self.match_list_store)
+    self.ActionsTree.set_model(self.actions_list_store)
+
     self.ActionsNames_column=gtk.TreeViewColumn('Action')
     self.ActionsEnable_column=gtk.TreeViewColumn('')
     self.ActionsTree.append_column(self.ActionsEnable_column)
@@ -138,14 +199,15 @@ class gdevilspie:
     self.MatchPropertyEnable_cell.connect("toggled", self.MatchPropertyEnable_toggle)
     self.MatchTree.connect("cursor-changed", self.MatchPropertyRow_selected)
     
-    self.RulesList.show_all()  
-  
+    self.RuleEdit.show_all()
+    
   def Actions_selected(self, widget):
     selected_row = self.ActionsTree.get_selection()
     (model, iter) = selected_row.get_selected()
     if (iter != None):
       path = model.get_string_from_iter(iter)
       self.ActionsParameters_notebook.set_current_page(int(path))
+      self.ActionsParameters_notebook.show()
     
   def ActionsEnable_toggle(self, widget, path):
     iter = self.actions_list_store.get_iter_from_string(path)
@@ -157,76 +219,47 @@ class gdevilspie:
     (model, iter) = selected_row.get_selected()
     if (iter != None):
       path = model.get_string_from_iter(iter)
-    self.MatchPropertyParameters_notebook.set_current_page(int(path))
+      self.MatchPropertyParameters_notebook.set_current_page(int(path))
   
   def MatchPropertyEnable_toggle(self, widget, path):
     iter = self.match_list_store.get_iter_from_string(path)
     CurrentState = self.match_list_store.get_value(iter, 0)
     self.match_list_store.set_value(iter, 0 , not CurrentState )
-    
-  def on_RulesList_destroy(self,widget):
-    gtk.main_quit()
-
-  def on_Quit_clicked(self, widget):
-    gtk.main_quit()
-
-  def on_AddRule_clicked(self,widget):
-    self.RuleEdit.show_all()
-
+  
   def on_RuleEdit_destroy(self,widget):
-    self.RuleEdit.hide()
-
+    self.RuleEdit.destroy()
+    
   def on_Cancel_clicked(self,widget):
-    self.RuleEdit.hide()
+    self.RuleEdit.destroy()
     
   def on_Save_clicked(self,widget):
    str = self.RuleName_entry.get_text()
    self.Save_Rule(str)
-   self.RuleEdit.hide()
-
+   
   def Save_Rule(self, str):
     #do stuff to generate the rule.
     path = os.path.expanduser("~/.devilspie/")
-    new_Rule_file_name = path + str + ".ds"
-    f = open( new_Rule_file_name, 'w' )
-    f.write( "# " + str )
-    f.close()
-    self.update_rules_list()
-
-  def on_DeleteRule_clicked(self,widget):
-   SelectedRow = self.RulesTree.get_selection()
-   (model, iter) = SelectedRow.get_selected()
-   if (iter != None):
-     SelectedRule = self.rules_list_store.get(iter, 0)
-     RuleFile = os.path.expanduser("~/.devilspie/") + SelectedRule[0] + '.ds'
-     os.remove(RuleFile)
-     self.rules_list_store.remove(iter)
-
-  def update_rules_list(self):
-    self.rules_list_store.clear()
-    self.fill_rules_list()
-    
-  def fill_rules_list(self):
-    rulefileslist = os.listdir(self.dir)
-    for rulefile in rulefileslist:
-      if (rulefile.endswith(".ds")):
-        rulefile=gobject.filename_display_name(rulefile)
-        rulefile=rulefile.replace(".ds","")
-        self.rules_list_store.append([rulefile])
-
-  def main(self):
-    self.dir = os.path.expanduser("~/.devilspie")
-    if (os.path.exists(self.dir)):
-      if (os.path.isdir(self.dir)):
-        self.fill_rules_list()
+    new_Rule_file_name = str + ".ds"
+    rulefileslist = os.listdir(dir)
+    if ( new_Rule_file_name in rulefileslist ):
+      error_dialog = gtk.MessageDialog(self.RuleEdit, gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_QUESTION, gtk.BUTTONS_YES_NO, "The rule name you entered is already in use, do you want to overwrite it?")
+      response = error_dialog.run()
+      error_dialog.destroy()
+      if ( response == gtk.RESPONSE_YES ):
+        try:
+          new_Rule_file_name = path + new_Rule_file_name
+          f = open( new_Rule_file_name, 'w' )
+          f.write( "# " + str )
+          f.close()
+          MainWindow.update_rules_list()
+          self.RuleEdit.destroy()
+        except:
+          error_dialog = gtk.MessageDialog(self.RuleEdit, gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_ERROR, gtk.BUTTONS_CANCEL, "Could not save the rule, please check file permissions and try again.")
+          response = error_dialog.run()
+          error_dialog.destroy()
       else:
-          print "~/.devilspie is a file, please remove it"
-    else:
-          os.makedirs(dir)
-    
-    
-    gtk.main()
+        pass
 
-if __name__ == "__main__":
-    prog=gdevilspie()
-    prog.main()
+
+MainWindow = RulesListWindow()
+gtk.main()
