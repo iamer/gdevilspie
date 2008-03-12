8524cfe3af2b34a976bda5ba109cdcb26d0e5d00
theSamo <theSamo@04f495f3-2145-0410-90fa-e36e85c9abab>
2008-03-12 21:31:40 +0000

    more changes to reader

--- reader.py
+++ reader.py
@@ -1,22 +1,8 @@
 import parser
+import string
 
-# open a file and pass it to parser.py
-foo = open('test3.ds')
-z = parser.parse_file(foo)
-# clean some of the parser crap
-rule = z[0]
-
-#the dicts we will use
-matdict = {}
-actiondict = {}
-
-#the reader part...
-if rule[0] == 'if':
-    matcond = z[0][1]
-    action = z[0][2]
-
-print matcond
-print action
+def geo_parse(gstring):
+	return
 
 def not_checker(matdict,item):
 	if item[0] == 'is':
@@ -36,23 +22,43 @@ def condition_checker(matdict,item):
 	elif item[0]== "not":
 		not_checker(matdict,item[1])
 
-if matcond[0] == "and":
-	for item in matcond:
-		condition_checker(matdict, item)
-else:
-	condition_checker(matdict, matcond)
-		
+def read_file(filename):
+# open a file and pass it to parser.py
+	foo = open(filename)
+	z = parser.parse_file(foo)
+# clean some of the parser crap
+	rule = z[0]
 
-if action[0] == 'begin':
-	#FIXME: Handle Actions correctly.
-	for i in action[1:]:
-		print i
-		if len(i) > 1:
-			actiondict[i[0]] = i[1]
-		else:
-			actiondict [i[0]] = "True"
-else:
-    actiondict['actions'] = action[0]
+#the dicts we will use
+	matdict = {}
+	actiondict = {}
 
-print matdict
-print actiondict
+#the reader part...
+	if rule[0] == 'if':
+		matcond = z[0][1]
+		action = z[0][2]
+
+	if matcond[0] == "and":
+		for item in matcond:
+			condition_checker(matdict, item)
+	else:
+		condition_checker(matdict, matcond)
+			
+
+	if action[0] == 'begin':
+		#FIXME: Handle Actions correctly.
+		for i in action[1:]:
+			if len(i) > 1:
+				actiondict[i[0]] = i[1]
+			elif i[0][:2] != "un":
+				actiondict[i[0]] = "True"
+			elif i[0][:2] == "un":
+				actiondict[i[0][2:]] = "False"
+				actiondict[i[0]] = "True"
+	else:
+		actiondict[action[0]] = action[1]
+
+	return [matdict,actiondict]
+
+print read_file("test.ds")
+geo_parse("FIXME: Write that part phaeron :P")
