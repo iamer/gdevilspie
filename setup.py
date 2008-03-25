5121c4724ae12ad85fa3c0bac8a0be196365fa33
theSamo <theSamo@04f495f3-2145-0410-90fa-e36e85c9abab>
2008-03-25 15:47:09 +0000

    Major renaming. only glade loading left for setup.py to work on installations.

--- setup.py
+++ setup.py
@@ -3,16 +3,15 @@
 from distutils.core import setup
 
 setup(name='gdevilspie',
-      version='0.1',
+      version='0.3',
       description='GTK GUI for devilspie',
       author='Islam Amer',
       author_email='iamer@open-craft.com',
       url='http://code.google.com/p/gdevilspie/',
 	  license='GPLv2',
       py_modules=['parser', 'reader','filler'],
-	  scripts=['./gdevilspie.py'],
-	  data_files=[('data',['./gdevilspie.glade', './window-new.png'])],
-     )
-#we need to clean the dir heirarchy; root includes only py files and README, icon and .glade go to data subdirs. also i dunno if the script is made system-wide excutable or not.
-#FIXME: Add data/ subdir and move non py stuff there.
+	  scripts=['gdevilspie'],
+	  data_files=[('share/gdevilspie',['gdevilspie.glade', 'gdevilspie.png']),
+		  ('share/pixmaps/',['gdevilspie.png'])],
+     ) 
 
