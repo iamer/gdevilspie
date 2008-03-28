3ffef4ac944a48a6519c2927b5b2081f7948f92b
theSamo <theSamo@04f495f3-2145-0410-90fa-e36e85c9abab>
2008-03-28 06:17:46 +0000

    Added README,COPYING, .desktop file. Removed MISC. Removed version data from all files in trunk. Added installing doc files and .dekstop to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='gdevilspie',
-      version='0.3',
+      version='0.0(svn)',
       description='GTK GUI for devilspie',
       author='Islam Amer',
       author_email='iamer@open-craft.com',
@@ -12,6 +12,8 @@ setup(name='gdevilspie',
       packages=['gDevilspie'],
 	  scripts=['gdevilspie'],
 	  data_files=[('share/gdevilspie',['gdevilspie.glade', 'gdevilspie.png']),
+          ('share/doc/gdevilspie',['README','Changelog','TODO']),
+          ('share/applications',['gdevilspie.desktop']),
 		  ('share/pixmaps/',['gdevilspie.png'])],
      ) 
-#FIXME: Add README, etc.	
+
